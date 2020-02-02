6d54af94f134a541ddbc02f9c823097093fce72a
Shailesh Ladumor <shaileshmladumor@gmail.com>
2020-02-02 14:05:50 +0530

    feat(notifications): retrieve all notifications support added

---
+++ src/OneSignalClient.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Ladumor\OneSignal;
+
+/**
+ * Class OneSignalClient
+ */
+class OneSignalClient
+{
+    /**
+     * return headers
+     * @return array
+     */
+    protected function getHeaders()
+    {
+        return array(
+            'Content-Type: application/json; charset=utf-8',
+            'Authorization: Basic '.config('one-signal.authorize'),
+        );
+    }
+
+    /**
+     * GEt Method
+     * @param string $url
+     * @return array|mixed
+     */
+    public function get($url)
+    {
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL            => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING       => "",
+                CURLOPT_MAXREDIRS      => 10,
+                CURLOPT_TIMEOUT        => 30,
+                CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST  => "GET",
+                CURLOPT_HTTPHEADER     => $this->getHeaders(),
+            ));
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+            if (!empty($err)) {
+                return json_decode($err, true);
+            }
+
+            return json_decode($response, true);
+        } catch (\Exception $exception) {
+            return [
+                'code'    => $exception->getCode(),
+                'message' => $exception->getMessage(),
+            ];
+        }
+    }
+
+    /**
+     * Post Method
+     * @param string $url
+     * @param string $fields
+     *
+     * @return array|mixed
+     */
+    public function post($url, $fields)
+    {
+        try {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $this->getHeaders());
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($ch);
+            $err = curl_error($ch);
+            curl_close($ch);
+
+            if (!empty($err)) { // return  error
+                return json_decode($err, true);
+            }
+
+            return json_decode($response, true); // return success
+        } catch (\Exception $exception) {
+            return [
+                'code'    => $exception->getCode(),
+                'message' => $exception->getMessage(),
+            ];
+        }
+    }
+}
